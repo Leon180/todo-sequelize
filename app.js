4849d7192068cd97ddb6522b9b648fd2515c3af8
Leon180 <lioung860302@gmail.com>
2023-02-24 17:07:12 +0800

    feat: add seeder and index view

--- app.js
+++ app.js
@@ -17,8 +17,17 @@ app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
-app.get('/', (req, res) => {
-  res.send('hello world')
+app.get('/', async (req, res) => {
+  try {
+    const todos = await Todo.findAll({
+      raw: true,
+      nest: true
+    })
+    return res.render('index', { todos: todos })
+  }
+  catch {
+    return res.status(422).json(error)
+  }
 })
 
 app.get('/users/login', (req, res) => {
