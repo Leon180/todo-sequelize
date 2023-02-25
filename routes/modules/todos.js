f89046449aa63c461351b8da5c000a0e3f7ad472
Leon180 <lioung860302@gmail.com>
2023-02-25 18:05:41 +0800

    feat: CRUD function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -3,18 +3,20 @@ const router = express.Router()
 const db = require('../../models')
 const Todo = db.Todo
 
+// create
 router.get('/new', (req, res) => {
   return res.render('new')
 })
-
-router.post('/', (req, res) => {
-  console.log(req.user)
-  return res.render('new')
-  // const userId = req.user._id
-  // const name = req.body.name
-  // return Todo.create({ name, userId })
-  //   .then(() => res.redirect('/'))
-  //   .catch(error => console.log(error))
+router.post('/', async (req, res) => {
+  const UserId = req.user.id
+  const name = req.body.name
+  try {
+    await Todo.create({ name, UserId })
+    res.redirect('/')
+  }
+  catch (error) {
+    console.log(error)
+  }
 })
 
 // detail
@@ -29,39 +31,48 @@ router.get('/:id', async (req, res) => {
   }
 })
 
-router.get('/:id/edit', (req, res) => {
-  return res.render('new')
-  // const userId = req.user._id
-  // const _id = req.params.id
-  // return Todo.findOne({ _id, userId })
-  //   .lean()
-  //   .then(todo => res.render('edit', { todo }))
-  //   .catch(error => console.log(error))
+// edit
+router.get('/:id/edit', async (req, res) => {
+  const id = req.params.id
+  try {
+    const todo = await Todo.findByPk(id)
+    return res.render('edit', { todo: todo.toJSON() })
+  }
+  catch (error) {
+    console.log(error)
+  }
+})
+router.put('/:id', async (req, res) => {
+  const id = req.params.id
+  const { name, isDone } = req.body
+  try {
+    await Todo.update({
+      name, isDone: isDone === 'on'
+    }, {
+      where: {
+        id
+      }
+    });
+    return res.redirect(`/todos/${id}`)
+  }
+  catch (error) {
+    console.log(error)
+  }
 })
 
-router.put('/:id', (req, res) => {
-  return res.render('new')
-  // const userId = req.user._id
-  // const _id = req.params.id
-  // const { name, isDone } = req.body
-  // return Todo.findOne({ _id, userId })
-  //   .then(todo => {
-  //     todo.name = name
-  //     todo.isDone = isDone === 'on'
-  //     return todo.save()
-  //   })
-  //   .then(() => res.redirect(`/todos/${_id}`))
-  //   .catch(error => console.log(error))
-})
-
-router.delete('/:id', (req, res) => {
-  return res.render('new')
-  // const userId = req.user._id
-  // const _id = req.params.id
-  // return Todo.findOne({ _id, userId })
-  //   .then(todo => todo.remove())
-  //   .then(() => res.redirect('/'))
-  //   .catch(error => console.log(error))
+router.delete('/:id', async (req, res) => {
+  const id = req.params.id
+  try {
+    await Todo.destroy({
+      where: {
+        id
+      }
+    });
+    res.redirect('/')
+  }
+  catch (error) {
+    console.log(error)
+  }
 })
 
 module.exports = router
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const db = require('../../models')
 const User = db.User
+
 // 引用 passport
 const passport = require('passport')
 const bcrypt = require('bcryptjs')
